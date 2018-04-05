cb4cfb50273bf82107f34fc56cdf0c8b6f056b1d
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-04-05 21:02:33 +0300

    Pipe usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,29 @@ export class AppComponent {
       instanceType: 'medium',
       name: 'Production Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
+      count:5
     },
     {
       instanceType: 'large',
       name: 'User Database',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
+      count:15
     },
     {
       instanceType: 'small',
       name: 'Development Server',
       status: 'offline',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
+      count:12
     },
     {
       instanceType: 'small',
       name: 'Testing Environment Server',
       status: 'stable',
-      started: new Date(15, 1, 2017)
+      started: new Date(15, 1, 2017),
+      count:23
     }
   ];
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
